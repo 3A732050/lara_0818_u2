551be8564652fba8208e6167b3a038ee465f4add
林駿綦 <r10101720307@gmail.com>
2020-10-26 03:59:29 +0800

    練習3-4：新增名為 hello.index 的 Route，並用 HelloController 回傳 views

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('home', [\App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('hello/{name?}', [\App\Http\Controllers\HelloController::class, 'index'])->name('hello.index');
